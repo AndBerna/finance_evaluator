4012d476b05fbfdac10bfbbe207720c0384f0978
Andre <andberna@outlook.es>
2023-04-29 19:32:55 +0200

    Add defaults path and load session state

--- pages/02_💰_Financial_Evaluator.py
+++ pages/02_💰_Financial_Evaluator.py
@@ -16,7 +16,6 @@ Date: 17/04/2023
 import numpy as np
 import streamlit as st
 
-
 # for k, v in st.session_state.items():
 #     st.session_state[k] = v
 st.set_page_config(
@@ -25,3 +24,20 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
+
+
+# Define the default file paths
+
+expenses_to_drop_file_path = "data\default_expenses_to_drop.json"
+income_to_drop_file_path = "data\default_income_to_drop.json"
+
+
+# Load the data from the session state
+try:
+    df = st.session_state["df"]
+    data = df.copy()
+except (KeyError, AttributeError):
+    st.error("Please load a file first")
+    st.stop()
+
+currency = st.session_state["currency"]
