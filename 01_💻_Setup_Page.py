92070c9fd868c94101744599c1230c791df3caf1
Andre <andberna@outlook.es>
2023-04-29 17:18:39 +0200

    Load  csv_format defaults

--- 01_💻_Setup_Page.py
+++ 01_💻_Setup_Page.py
@@ -13,3 +13,12 @@ sys.path.append(os.path.join(dir_path, ".."))
 import text.setup_text as text
 from utils.data_tools import format_dataframe, load_csv
 from utils.streamlit_utils import save_as_default_button
+
+default_file_path = "data\default_csv_format.json"
+
+# Get the default value from session state or set an empty dict as the default
+try:
+    with open(default_file_path, "r") as f:
+        default_values = json.load(f)
+except FileNotFoundError:
+    default_values = {}
